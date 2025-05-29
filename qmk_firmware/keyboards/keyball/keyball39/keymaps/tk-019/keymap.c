98411d0d8367bc4700495d6c0c44b4ceb47198fb
gunshop-eikoh <toshiki-komatsu@eikoh-gun.com>
2025-05-30 02:23:27 +0900

    fix

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/tk-019/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/tk-019/keymap.c
@@ -25,17 +25,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default (VIA)
   [0] = LAYOUT_universal(
     KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
-    KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , KC_ENT  ,
-    KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , LT(1,KC_M)     , KC_COMM  , KC_DOT   , KC_RSFT  ,
+    LCTL_T(KC_A)     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , KC_ENT  ,
+    LSFT_T(KC_Z)  , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , LT(1,KC_M)     , KC_COMM  , KC_DOT   , KC_RSFT  ,
     KC_ESC  , KC_LALT  , KC_LGUI
     // 左親指
     ,KC_LCTL, LT(1,KC_LNG2),LT(2,KC_SPC)
     // 右親指
-    ,KC_BSPC,LT(2,KC_ENT),LT(3,KC_LNG1)
+    ,LT(1,KC_ENT),LT(2,KC_LNG1)
     // 不要
-    ,_______,_______,_______
+    ,_______,_______
     // 右下端
-    , KC_RSFT
+    , KC_LNG3
   ),
 
   // functionと左移動
