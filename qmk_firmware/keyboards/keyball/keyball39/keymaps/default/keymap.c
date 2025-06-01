81264101fe5a62531a5689585114276aa9d300e4
gunshop-eikoh <toshiki-komatsu@eikoh-gun.com>
2025-06-01 23:35:57 +0900

    keymap: update combo definitions for improved functionality

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/default/keymap.c
@@ -26,14 +26,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
     KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
     LCTL_T(KC_A)     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , KC_ENT  ,
-    LSFT_T(KC_Z)  , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , LT(3,KC_M)     , KC_COMM  , LT(2, KC_DOT)   , RSFT_T(S(KC_INT1))  ,
+    LSFT_T(KC_Z)  , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , LT(3,KC_M)     , KC_COMM  , LT(2, KC_DOT)   , RSFT_T(S(KC_SLSH))  ,
     KC_ESC  , KC_LALT  , KC_LGUI
     // 左親指
     ,KC_LCTL, LT(1,KC_LNG2),LT(2,KC_SPC)
     // 右親指
     ,LT(1,KC_SPC),LT(2,KC_LNG1)
     // 右下端
-    , KC_LNG3
+    , LT(3,S(KC_INT1))
   ),
   // 左移動とファンクション
   [1] = LAYOUT(
