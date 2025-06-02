75cb64ad3445fc00b3713b40086153bf626b8f10
gunshop-eikoh <toshiki-komatsu@eikoh-gun.com>
2025-06-02 12:01:55 +0900

    keymap: refine combo definitions for enhanced functionality and clarity

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/default/keymap.c
@@ -26,27 +26,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
     KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
     LCTL_T(KC_A)     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , KC_ENT  ,
-    LSFT_T(KC_Z)  , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , LT(3,KC_M)     , KC_COMM  , LT(2, KC_DOT)   , RSFT_T(S(KC_SLSH))  ,
+    LSFT_T(KC_Z)  , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , LT(3,KC_M)     , KC_COMM  , LT(2, KC_DOT)   , RSFT_T(LSFT(KC_SLSH))  ,
     KC_ESC  , KC_LALT  , KC_LGUI
     // 左親指
     ,KC_LCTL, LT(1,KC_LNG2),LT(2,KC_SPC)
     // 右親指
     ,LT(1,KC_SPC),LT(2,KC_LNG1)
     // 右下端
-    , LT(3,S(KC_INT1))
+    , LT(3,LSFT(KC_INT1))
   ),
   // 左移動とファンクション
   [1] = LAYOUT(
     KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5  ,                            KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   ,
-    KC_LCTL    , C(G(KC_LEFT))  , G(KC_TAB)  ,C(G(KC_RGHT)), S(KC_8)  ,                           S(KC_INT1), KC_BTN1  , KC_PGUP  , KC_BTN2  , KC_ENT  ,
-    KC_LSFT, LALT(KC_LEFT),A(KC_TAB)   , LALT(KC_RGHT)  ,S(KC_RBRC),                            KC_LBRC  , KC_DLR   , KC_PGDN  , KC_BTN3  , RSFT_T(KC_F11)   ,
+    KC_LCTL    , LCTL(LGUI(KC_LEFT))  , LGUI(KC_TAB)  ,LCTL(LGUI(KC_RGHT)), LSFT(KC_8)  ,                           LSFT(KC_INT1), KC_BTN1  , KC_PGUP  , KC_BTN2  , KC_ENT  ,
+    KC_LSFT, LALT(KC_LEFT),LALT(KC_TAB)   , LALT(KC_RGHT)  ,LSFT(KC_RBRC),                            KC_LBRC  , KC_DLR   , KC_PGDN  , KC_BTN3  , RSFT_T(KC_F11)   ,
     KC_ESC  , KC_LALT  , KC_LGUI  , KC_LCTL  , _______  , _______  ,      TO(2)    , TO(0) ,                                              KC_F12
   ),
   // 右移動＋マウスと左数字
   [2] = LAYOUT(
     KC_TAB   , KC_7     , KC_8     , KC_9     , KC_MINS  ,                            KC_PGUP  , KC_HOME  , KC_UP  , KC_END  , KC_PGDN  ,
-    LCTL_T(S(KC_QUOT)), KC_4     , KC_5     , KC_6     ,S(KC_SCLN),                            S(KC_9)  , KC_LEFT  , KC_DOWN    , KC_RGHT  , KC_ENT  ,
-    LSFT_T(KC_SLSH)  , KC_1     , KC_2     , KC_3     ,S(KC_MINS),                           KC_BTN3, KC_BTN1  , KC_BTN2  ,  _______ , KC_RSFT  ,
+    KC_LCTL, KC_4     , KC_5     , KC_6     ,LSFT(KC_SCLN),                            LSFT(KC_9)  , KC_LEFT  , KC_DOWN    , KC_RGHT  , KC_ENT  ,
+    KC_LSFT , KC_1     , KC_2     , KC_3     ,LSFT(KC_MINS),                           KC_BTN3, KC_BTN1  , KC_BTN2  ,  _______ , KC_RSFT  ,
     KC_ESC   , LALT_T(KC_0)     , ALT_T(KC_DOT)   , KC_LCTL   , KC_ENT   , KC_BSPC  ,      _______  , _______,                                   _______
   ),
   [3] = LAYOUT(
@@ -120,32 +120,32 @@ combo_t key_combos[] = {
     COMBO(combo_sd, KC_TAB), // tab2
     COMBO(combo_io, KC_BSPC), // bs
     COMBO(combo_op, KC_MINS), // -
-    COMBO(combo_qj, S(KC_7)), // '
-    COMBO(combo_wj, S(KC_2)), // "
+    COMBO(combo_qj, LSFT(KC_7)), // '
+    COMBO(combo_wj, LSFT(KC_2)), // "
     COMBO(combo_ej, LSFT(KC_1)), // !
     COMBO(combo_rj, KC_QUOT), // :
     COMBO(combo_tj, KC_SCLN), // ;
     COMBO(combo_yf, KC_INT3), // ¥
-    COMBO(combo_uf, S(KC_SCLN)), // +
-    COMBO(combo_if, S(KC_MINS)), // =
-    COMBO(combo_of, S(KC_INT3)), // |
-    COMBO(combo_fp, S(KC_5)), // %
+    COMBO(combo_uf, LSFT(KC_SCLN)), // +
+    COMBO(combo_if, LSFT(KC_MINS)), // =
+    COMBO(combo_of, LSFT(KC_INT3)), // |
+    COMBO(combo_fp, LSFT(KC_5)), // %
     COMBO(combo_aj, KC_LBRC), // @
     COMBO(combo_sj, KC_SLSH), // /
     COMBO(combo_dj, LSFT(KC_4)), // $
-    COMBO(combo_fj, S(KC_SLSH)), // ?
+    COMBO(combo_fj, LSFT(KC_SLSH)), // ?
     COMBO(combo_gj, LSFT(KC_LBRC)), // `(バッククォート)
-    COMBO(combo_hf, S(KC_3)), // #
+    COMBO(combo_hf, LSFT(KC_3)), // #
     COMBO(combo_kf, KC_RBRC), // [
     COMBO(combo_lf, KC_BSLS), // ]
-    COMBO(combo_zj, S(KC_EQL)), // ~
-    COMBO(combo_xj, S(KC_QUOT)), // *
+    COMBO(combo_zj, LSFT(KC_EQL)), // ~
+    COMBO(combo_xj, LSFT(KC_QUOT)), // *
     COMBO(combo_cj, KC_EQL), // ^
     COMBO(combo_bj, KC_INT1), /* \ */
-    COMBO(combo_nf, S(KC_6)), // &
+    COMBO(combo_nf, LSFT(KC_6)), // &
     COMBO(combo_mf, KC_MINS), // -
-    COMBO(combo_zx, S(KC_Z)), // shift z
-    COMBO(combo_commF, S(KC_8)), // (
-    COMBO(combo_dotF, S(KC_9)), // )
+    COMBO(combo_zx, LSFT(KC_Z)), // shift z
+    COMBO(combo_commF, LSFT(KC_8)), // (
+    COMBO(combo_dotF, LSFT(KC_9)), // )
 };
 #endif
