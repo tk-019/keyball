242743c6467459c1c5d4ed5439281b78145ac9a0
gunshop-eikoh <toshiki-komatsu@eikoh-gun.com>
2025-06-04 00:49:14 +0900

    keymap: add new combo definition for "K + J" to trigger Shift+Win+← action

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/default/keymap.c
@@ -78,141 +78,280 @@ void oledkit_render_info_user(void) {
 
 #ifdef COMBO_ENABLE
 
-// 1) まず、各コンボの「キーの組み合わせ」を PROGMEM に定義。
-//    PROGMEM にしておくと、RAM に置きたくない部分（実キーコードの列）だけを
-//    フラッシュに置いたままにできます。
-static const uint16_t PROGMEM combo_jk[]    = { KC_J,    KC_K,    COMBO_END };  // BTN1
-static const uint16_t PROGMEM combo_kl[]    = { KC_K,    KC_L,    COMBO_END };  // BTN2
-static const uint16_t PROGMEM combo_jl[]    = { KC_J,    KC_L,    COMBO_END };  // BTN3
-static const uint16_t PROGMEM combo_qw[]    = { KC_Q,    KC_W,    COMBO_END };  // TAB
-static const uint16_t PROGMEM combo_sd[]    = { KC_S,    KC_D,    COMBO_END };  // TAB2
-static const uint16_t PROGMEM combo_io[]    = { KC_I,    KC_O,    COMBO_END };  // BACKSPACE
-static const uint16_t PROGMEM combo_op[]    = { KC_O,    KC_P,    COMBO_END };  // “-”
-static const uint16_t PROGMEM combo_qj[]    = { KC_Q,    KC_J,    COMBO_END };  // “’” (Shift+7)
-static const uint16_t PROGMEM combo_wj[]    = { KC_W,    KC_J,    COMBO_END };  // “"” (Shift+2)
-static const uint16_t PROGMEM combo_ej[]    = { KC_E,    KC_J,    COMBO_END };  // “!” (Shift+1)
-static const uint16_t PROGMEM combo_rj[]    = { KC_R,    KC_J,    COMBO_END };  // “:” (QUOT)
-static const uint16_t PROGMEM combo_tj[]    = { KC_T,    KC_J,    COMBO_END };  // “;” (SCLN)
-static const uint16_t PROGMEM combo_yf[]    = { KC_Y,    KC_F,    COMBO_END };  // “¥” (INT3)
-static const uint16_t PROGMEM combo_uf[]    = { KC_U,    KC_F,    COMBO_END };  // “+” (Shift+SCLN)
-static const uint16_t PROGMEM combo_if[]    = { KC_I,    KC_F,    COMBO_END };  // “=” (Shift+MINS)
-static const uint16_t PROGMEM combo_of[]    = { KC_O,    KC_F,    COMBO_END };  // “|” (Shift+INT3)
-static const uint16_t PROGMEM combo_fp[]    = { KC_P,    KC_F,    COMBO_END };  // “%” (Shift+5)
-static const uint16_t PROGMEM combo_aj[]    = { KC_A,    KC_J,    COMBO_END };  // “@” (LBRC)
-static const uint16_t PROGMEM combo_sj[]    = { KC_S,    KC_J,    COMBO_END };  // “/” (SLSH)
-static const uint16_t PROGMEM combo_dj[]    = { KC_D,    KC_J,    COMBO_END };  // “$” (Shift+4)
-static const uint16_t PROGMEM combo_fj[]    = { KC_F,    KC_J,    COMBO_END };  // “?” (Shift+SLSH)
-static const uint16_t PROGMEM combo_gj[]    = { KC_G,    KC_J,    COMBO_END };  // “`” (Shift+LBRC)
-static const uint16_t PROGMEM combo_hf[]    = { KC_H,    KC_F,    COMBO_END };  // “#” (Shift+3)
-static const uint16_t PROGMEM combo_kf[]    = { KC_K,    KC_F,    COMBO_END };  // “[” (RBRC)
-static const uint16_t PROGMEM combo_lf[]    = { KC_L,    KC_F,    COMBO_END };  // “]” (BSLS)
-static const uint16_t PROGMEM combo_zj[]    = { KC_Z,    KC_J,    COMBO_END };  // “~” (Shift+EQL)
-static const uint16_t PROGMEM combo_xj[]    = { KC_X,    KC_J,    COMBO_END };  // “*” (Shift+QUOT)
-static const uint16_t PROGMEM combo_cj[]    = { KC_C,    KC_J,    COMBO_END };  // “^” (EQL)
-static const uint16_t PROGMEM combo_bj[]    = { KC_B,    KC_J,    COMBO_END };  // “\” (INT1)
-static const uint16_t PROGMEM combo_nf[]    = { KC_N,    KC_F,    COMBO_END };  // “&” (Shift+6)
-static const uint16_t PROGMEM combo_mf[]    = { KC_M,    KC_F,    COMBO_END };  // “-” (MINS)
-static const uint16_t PROGMEM combo_zx[]    = { KC_Z,    KC_X,    COMBO_END };  // Shift+Z
-static const uint16_t PROGMEM combo_commF[] = { KC_COMM, KC_F,    COMBO_END };  // “(” (Shift+8)
-static const uint16_t PROGMEM combo_dotF[]  = { KC_DOT,  KC_F,    COMBO_END };  // “)” (Shift+9)
+// 1) “K + J で Shift+Win+←” 用のコンボ定義
+const uint16_t PROGMEM combo_kj_swl[] = { KC_K, KC_J, COMBO_END };
 
+// 既存のコンボ定義もそのまま並べてください
+const uint16_t PROGMEM combo_jk[]    = { KC_J, KC_K, COMBO_END };    // btn1
+const uint16_t PROGMEM combo_kl[]    = { KC_K, KC_L, COMBO_END };    // btn2
+const uint16_t PROGMEM combo_jl[]    = { KC_J, KC_L, COMBO_END };    // btn3
+const uint16_t PROGMEM combo_qw[]    = { KC_Q, KC_W, COMBO_END };    // tab
+const uint16_t PROGMEM combo_sd[]    = { KC_S, KC_D, COMBO_END };    // tab2
+const uint16_t PROGMEM combo_io[]    = { KC_I, KC_O, COMBO_END };    // bs
+const uint16_t PROGMEM combo_op[]    = { KC_O, KC_P, COMBO_END };    // ー／-
+const uint16_t PROGMEM combo_qj[]    = { KC_Q, KC_J, COMBO_END };    // '
+const uint16_t PROGMEM combo_wj[]    = { KC_W, KC_J, COMBO_END };    // "
+const uint16_t PROGMEM combo_ej[]    = { KC_E, KC_J, COMBO_END };    // !
+const uint16_t PROGMEM combo_rj[]    = { KC_R, KC_J, COMBO_END };    // :
+const uint16_t PROGMEM combo_tj[]    = { KC_T, KC_J, COMBO_END };    // ;
+const uint16_t PROGMEM combo_yf[]    = { KC_Y, KC_F, COMBO_END };    // ¥
+const uint16_t PROGMEM combo_uf[]    = { KC_U, KC_F, COMBO_END };    // +
+const uint16_t PROGMEM combo_if[]    = { KC_I, KC_F, COMBO_END };    // =
+const uint16_t PROGMEM combo_of[]    = { KC_O, KC_F, COMBO_END };    // |
+const uint16_t PROGMEM combo_fp[]    = { KC_P, KC_F, COMBO_END };    // %
+const uint16_t PROGMEM combo_aj[]    = { KC_A, KC_J, COMBO_END };    // @
+const uint16_t PROGMEM combo_sj[]    = { KC_S, KC_J, COMBO_END };    // /
+const uint16_t PROGMEM combo_dj[]    = { KC_D, KC_J, COMBO_END };    // $
+const uint16_t PROGMEM combo_fj[]    = { KC_F, KC_J, COMBO_END };    // ?
+const uint16_t PROGMEM combo_gj[]    = { KC_G, KC_J, COMBO_END };    // `
+const uint16_t PROGMEM combo_hf[]    = { KC_H, KC_F, COMBO_END };    // #
+const uint16_t PROGMEM combo_kf[]    = { KC_K, KC_F, COMBO_END };    // [
+const uint16_t PROGMEM combo_lf[]    = { KC_L, KC_F, COMBO_END };    // ]
+const uint16_t PROGMEM combo_zj[]    = { KC_Z, KC_J, COMBO_END };    // ~
+const uint16_t PROGMEM combo_xj[]    = { KC_X, KC_J, COMBO_END };    // *
+const uint16_t PROGMEM combo_cj[]    = { KC_C, KC_J, COMBO_END };    // ^
+const uint16_t PROGMEM combo_bj[]    = { KC_B, KC_J, COMBO_END };    // \
+const uint16_t PROGMEM combo_nf[]    = { KC_N, KC_F, COMBO_END };    // &
+const uint16_t PROGMEM combo_mf[]    = { KC_M, KC_F, COMBO_END };    // -
+const uint16_t PROGMEM combo_zx[]    = { KC_Z, KC_X, COMBO_END };    // Shift+Z（例）
+const uint16_t PROGMEM combo_commF[] = { KC_COMM, KC_F, COMBO_END }; // (
+const uint16_t PROGMEM combo_dotF[]  = { KC_DOT, KC_F, COMBO_END };  // )
 
-// 2) 「コンボのインデックス」を列挙型で定義
+// 2) コンボのインデックスを列挙(enum)に追加
 enum combo_index {
-    C_JK,      // combo_jk
-    C_KL,      // combo_kl
-    C_JL,      // combo_jl
-    C_QW,      // combo_qw
-    C_SD,      // combo_sd
-    C_IO,      // combo_io
-    C_OP,      // combo_op
-    C_QJ,      // combo_qj
-    C_WJ,      // combo_wj
-    C_EJ,      // combo_ej
-    C_RJ,      // combo_rj
-    C_TJ,      // combo_tj
-    C_YF,      // combo_yf
-    C_UF,      // combo_uf
-    C_IF,      // combo_if
-    C_OF,      // combo_of
-    C_FP,      // combo_fp
-    C_AJ,      // combo_aj
-    C_SJ,      // combo_sj
-    C_DJ,      // combo_dj
-    C_FJ,      // combo_fj
-    C_GJ,      // combo_gj
-    C_HF,      // combo_hf
-    C_KF,      // combo_kf
-    C_LF,      // combo_lf
-    C_ZJ,      // combo_zj
-    C_XJ,      // combo_xj
-    C_CJ,      // combo_cj
-    C_BJ,      // combo_bj
-    C_NF,      // combo_nf
-    C_MF,      // combo_mf
-    C_ZX,      // combo_zx
-    C_COMMF,   // combo_commF
-    C_DOTF,    // combo_dotF
-    C_NUM      // 合計数（必ず最後）
+    C_JK,
+    C_KL,
+    C_JL,
+    C_QW,
+    C_SD,
+    C_IO,
+    C_OP,
+    C_QJ,
+    C_WJ,
+    C_EJ,
+    C_RJ,
+    C_TJ,
+    C_YF,
+    C_UF,
+    C_IF,
+    C_OF,
+    C_FP,
+    C_AJ,
+    C_SJ,
+    C_DJ,
+    C_FJ,
+    C_GJ,
+    C_HF,
+    C_KF,
+    C_LF,
+    C_ZJ,
+    C_XJ,
+    C_CJ,
+    C_BJ,
+    C_NF,
+    C_MF,
+    C_ZX,
+    C_COMM_F,
+    C_DOT_F,
+    // ────── ここに新規追加 ──────
+    C_KJ_SWL,
+    COMBO_COUNT
 };
 
-
-// 3) RAM 上に展開される combo_t 配列を、「必ずこのイニシャライザの中だけで」 COMBO(...) を使って定義する
-//    → このブロック全体が「= { … };」 のイニシャライザ部分であることに注意
-combo_t key_combos[C_NUM] = {
-    [C_JK]     = COMBO( combo_jk,    COMBO_ACTION(C_JK) ),
-    [C_KL]     = COMBO( combo_kl,    COMBO_ACTION(C_KL) ),
-    [C_JL]     = COMBO( combo_jl,    COMBO_ACTION(C_JL) ),
-    [C_QW]     = COMBO( combo_qw,    COMBO_ACTION(C_QW) ),
-    [C_SD]     = COMBO( combo_sd,    COMBO_ACTION(C_SD) ),
-    [C_IO]     = COMBO( combo_io,    COMBO_ACTION(C_IO) ),
-    [C_OP]     = COMBO( combo_op,    COMBO_ACTION(C_OP) ),
-    [C_QJ]     = COMBO( combo_qj,    COMBO_ACTION(C_QJ) ),
-    [C_WJ]     = COMBO( combo_wj,    COMBO_ACTION(C_WJ) ),
-    [C_EJ]     = COMBO( combo_ej,    COMBO_ACTION(C_EJ) ),
-    [C_RJ]     = COMBO( combo_rj,    COMBO_ACTION(C_RJ) ),
-    [C_TJ]     = COMBO( combo_tj,    COMBO_ACTION(C_TJ) ),
-    [C_YF]     = COMBO( combo_yf,    COMBO_ACTION(C_YF) ),
-    [C_UF]     = COMBO( combo_uf,    COMBO_ACTION(C_UF) ),
-    [C_IF]     = COMBO( combo_if,    COMBO_ACTION(C_IF) ),
-    [C_OF]     = COMBO( combo_of,    COMBO_ACTION(C_OF) ),
-    [C_FP]     = COMBO( combo_fp,    COMBO_ACTION(C_FP) ),
-    [C_AJ]     = COMBO( combo_aj,    COMBO_ACTION(C_AJ) ),
-    [C_SJ]     = COMBO( combo_sj,    COMBO_ACTION(C_SJ) ),
-    [C_DJ]     = COMBO( combo_dj,    COMBO_ACTION(C_DJ) ),
-    [C_FJ]     = COMBO( combo_fj,    COMBO_ACTION(C_FJ) ),
-    [C_GJ]     = COMBO( combo_gj,    COMBO_ACTION(C_GJ) ),
-    [C_HF]     = COMBO( combo_hf,    COMBO_ACTION(C_HF) ),
-    [C_KF]     = COMBO( combo_kf,    COMBO_ACTION(C_KF) ),
-    [C_LF]     = COMBO( combo_lf,    COMBO_ACTION(C_LF) ),
-    [C_ZJ]     = COMBO( combo_zj,    COMBO_ACTION(C_ZJ) ),
-    [C_XJ]     = COMBO( combo_xj,    COMBO_ACTION(C_XJ) ),
-    [C_CJ]     = COMBO( combo_cj,    COMBO_ACTION(C_CJ) ),
-    [C_BJ]     = COMBO( combo_bj,    COMBO_ACTION(C_BJ) ),
-    [C_NF]     = COMBO( combo_nf,    COMBO_ACTION(C_NF) ),
-    [C_MF]     = COMBO( combo_mf,    COMBO_ACTION(C_MF) ),
-    [C_ZX]     = COMBO( combo_zx,    COMBO_ACTION(C_ZX) ),
-    [C_COMMF]  = COMBO( combo_commF, COMBO_ACTION(C_COMMF) ),
-    [C_DOTF]   = COMBO( combo_dotF,  COMBO_ACTION(C_DOTF) ),
+// 3) key_combos[] に COMBO_ACTION() で登録
+combo_t key_combos[COMBO_COUNT] = {
+    [C_JK]      = COMBO(combo_jk,    COMBO_ACTION(C_JK)),
+    [C_KL]      = COMBO(combo_kl,    COMBO_ACTION(C_KL)),
+    [C_JL]      = COMBO(combo_jl,    COMBO_ACTION(C_JL)),
+    [C_QW]      = COMBO(combo_qw,    COMBO_ACTION(C_QW)),
+    [C_SD]      = COMBO(combo_sd,    COMBO_ACTION(C_SD)),
+    [C_IO]      = COMBO(combo_io,    COMBO_ACTION(C_IO)),
+    [C_OP]      = COMBO(combo_op,    COMBO_ACTION(C_OP)),
+    [C_QJ]      = COMBO(combo_qj,    COMBO_ACTION(C_QJ)),
+    [C_WJ]      = COMBO(combo_wj,    COMBO_ACTION(C_WJ)),
+    [C_EJ]      = COMBO(combo_ej,    COMBO_ACTION(C_EJ)),
+    [C_RJ]      = COMBO(combo_rj,    COMBO_ACTION(C_RJ)),
+    [C_TJ]      = COMBO(combo_tj,    COMBO_ACTION(C_TJ)),
+    [C_YF]      = COMBO(combo_yf,    COMBO_ACTION(C_YF)),
+    [C_UF]      = COMBO(combo_uf,    COMBO_ACTION(C_UF)),
+    [C_IF]      = COMBO(combo_if,    COMBO_ACTION(C_IF)),
+    [C_OF]      = COMBO(combo_of,    COMBO_ACTION(C_OF)),
+    [C_FP]      = COMBO(combo_fp,    COMBO_ACTION(C_FP)),
+    [C_AJ]      = COMBO(combo_aj,    COMBO_ACTION(C_AJ)),
+    [C_SJ]      = COMBO(combo_sj,    COMBO_ACTION(C_SJ)),
+    [C_DJ]      = COMBO(combo_dj,    COMBO_ACTION(C_DJ)),
+    [C_FJ]      = COMBO(combo_fj,    COMBO_ACTION(C_FJ)),
+    [C_GJ]      = COMBO(combo_gj,    COMBO_ACTION(C_GJ)),
+    [C_HF]      = COMBO(combo_hf,    COMBO_ACTION(C_HF)),
+    [C_KF]      = COMBO(combo_kf,    COMBO_ACTION(C_KF)),
+    [C_LF]      = COMBO(combo_lf,    COMBO_ACTION(C_LF)),
+    [C_ZJ]      = COMBO(combo_zj,    COMBO_ACTION(C_ZJ)),
+    [C_XJ]      = COMBO(combo_xj,    COMBO_ACTION(C_XJ)),
+    [C_CJ]      = COMBO(combo_cj,    COMBO_ACTION(C_CJ)),
+    [C_BJ]      = COMBO(combo_bj,    COMBO_ACTION(C_BJ)),
+    [C_NF]      = COMBO(combo_nf,    COMBO_ACTION(C_NF)),
+    [C_MF]      = COMBO(combo_mf,    COMBO_ACTION(C_MF)),
+    [C_ZX]      = COMBO(combo_zx,    COMBO_ACTION(C_ZX)),
+    [C_COMM_F]  = COMBO(combo_commF, COMBO_ACTION(C_COMM_F)),
+    [C_DOT_F]   = COMBO(combo_dotF,  COMBO_ACTION(C_DOT_F)),
+    // ────── 新規追加 ──────
+    [C_KJ_SWL]  = COMBO(combo_kj_swl, COMBO_ACTION(C_KJ_SWL)),
 };
 
-#endif  // COMBO_ENABLE
+bool process_combo_event(uint16_t combo_index, bool pressed) {
+    // “離したとき” は無視
+    if (!pressed) return true;
 
+    switch (combo_index) {
+        // ──── 既存のコンボ動作 ────
+        case C_JK:
+            tap_code(KC_BTN1);
+            break;
+        case C_KL:
+            tap_code(KC_BTN2);
+            break;
+        case C_JL:
+            tap_code(KC_BTN3);
+            break;
+        case C_QW:
+            tap_code(KC_TAB);
+            break;
+        case C_SD:
+            tap_code(KC_TAB);
+            break;
+        case C_IO:
+            tap_code(KC_BSPC);
+            break;
+        case C_OP:
+            tap_code(KC_MINS);
+            break;
+        case C_QJ:
+            register_mods(MOD_MASK_SHIFT);
+            tap_code(KC_7);
+            unregister_mods(MOD_MASK_SHIFT);
+            break;
+        case C_WJ:
+            register_mods(MOD_MASK_SHIFT);
+            tap_code(KC_2);
+            unregister_mods(MOD_MASK_SHIFT);
+            break;
+        case C_EJ:
+            register_mods(MOD_MASK_SHIFT);
+            tap_code(KC_1);
+            unregister_mods(MOD_MASK_SHIFT);
+            break;
+        case C_RJ:
+            tap_code(KC_QUOT);
+            break;
+        case C_TJ:
+            tap_code(KC_SCLN);
+            break;
+        case C_YF:
+            tap_code(KC_INT3);  // “¥”
+            break;
+        case C_UF:
+            register_mods(MOD_MASK_SHIFT);
+            tap_code(KC_SCLN);  // “＋”
+            unregister_mods(MOD_MASK_SHIFT);
+            break;
+        case C_IF:
+            register_mods(MOD_MASK_SHIFT);
+            tap_code(KC_MINS);  // “＝”
+            unregister_mods(MOD_MASK_SHIFT);
+            break;
+        case C_OF:
+            register_mods(MOD_MASK_SHIFT);
+            tap_code(KC_INT3);  // “｜”
+            unregister_mods(MOD_MASK_SHIFT);
+            break;
+        case C_FP:
+            register_mods(MOD_MASK_SHIFT);
+            tap_code(KC_5);     // “％”
+            unregister_mods(MOD_MASK_SHIFT);
+            break;
+        case C_AJ:
+            tap_code(KC_LBRC);   // “@” JIS では左角括弧キー
+            break;
+        case C_SJ:
+            tap_code(KC_SLSH);   // “/”
+            break;
+        case C_DJ:
+            register_mods(MOD_MASK_SHIFT);
+            tap_code(KC_4);     // “$”
+            unregister_mods(MOD_MASK_SHIFT);
+            break;
+        case C_FJ:
+            register_mods(MOD_MASK_SHIFT);
+            tap_code(KC_SLSH);  // “?”
+            unregister_mods(MOD_MASK_SHIFT);
+            break;
+        case C_GJ:
+            register_mods(MOD_MASK_SHIFT);
+            tap_code(KC_LBRC);  // “`” JIS では Shift+[ でバククォート
+            unregister_mods(MOD_MASK_SHIFT);
+            break;
+        case C_HF:
+            register_mods(MOD_MASK_SHIFT);
+            tap_code(KC_3);     // “#”
+            unregister_mods(MOD_MASK_SHIFT);
+            break;
+        case C_KF:
+            tap_code(KC_RBRC);   // “[” JIS では右角括弧キー
+            break;
+        case C_LF:
+            tap_code(KC_BSLS);   // “]” JIS ではバックスラッシュキー
+            break;
+        case C_ZJ:
+            register_mods(MOD_MASK_SHIFT);
+            tap_code(KC_EQL);   // “~”
+            unregister_mods(MOD_MASK_SHIFT);
+            break;
+        case C_XJ:
+            register_mods(MOD_MASK_SHIFT);
+            tap_code(KC_QUOT);  // “*”
+            unregister_mods(MOD_MASK_SHIFT);
+            break;
+        case C_CJ:
+            tap_code(KC_EQL);    // “^”
+            break;
+        case C_BJ:
+            tap_code(KC_INT1);   // “\” JIS では無変換キー
+            break;
+        case C_NF:
+            register_mods(MOD_MASK_SHIFT);
+            tap_code(KC_6);      // “&”
+            unregister_mods(MOD_MASK_SHIFT);
+            break;
+        case C_MF:
+            tap_code(KC_MINS);   // “-”
+            break;
+        case C_ZX:
+            register_mods(MOD_MASK_SHIFT);
+            tap_code(KC_Z);      // “Shift+Z”（用途例）
+            unregister_mods(MOD_MASK_SHIFT);
+            break;
+        case C_COMM_F:
+            register_mods(MOD_MASK_SHIFT);
+            tap_code(KC_8);      // “(”
+            unregister_mods(MOD_MASK_SHIFT);
+            break;
+        case C_DOT_F:
+            register_mods(MOD_MASK_SHIFT);
+            tap_code(KC_9);      // “)”
+            unregister_mods(MOD_MASK_SHIFT);
+            break;
 
-#ifdef COMBO_ENABLE
-// 4) process_combo_event() は必ず QMK と同じシグネチャ（void 戻り値）で定義する
-void process_combo_event(uint16_t combo_index, bool pressed) {
-    if (!pressed) return;
+        // ──── 新規: “Shift+Win+←” を送る ────
+        case C_KJ_SWL:
+            register_mods(MOD_MASK_SHIFT | MOD_MASK_GUI);
+            tap_code(KC_LEFT);
+            unregister_mods(MOD_MASK_SHIFT | MOD_MASK_GUI);
+            break;
 
-    // 送出キー＋修飾ビットテーブル（省略）
-    // 例：combo_output_table[C_JK] = { KC_BTN1, 0 };
-    //     combo_output_table[C_QJ] = { KC_7, MOD_BIT(KC_LSFT) }; などを別途定義しておく
-
-    extern const combo_output_t combo_output_table[];  // あなたが定義しているテーブルを参照
-    combo_output_t out = combo_output_table[combo_index];
-
-    if (out.mods)   register_mods  (out.mods);
-    tap_code16(out.keycode);
-    if (out.mods)   unregister_mods(out.mods);
+        default:
+            break;
+    }
+    return true;
 }
-#endif
+
+#endif // COMBO_ENABLE
