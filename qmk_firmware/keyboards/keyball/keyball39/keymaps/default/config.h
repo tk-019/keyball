12263b39ab4e70901e27c4761a2c2aae345e6015
gunshop-eikoh <toshiki-komatsu@eikoh-gun.com>
2025-06-11 01:27:58 +0900

    keymap: add COMBO_TERM definition for improved combo handling

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/default/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/default/config.h
@@ -27,4 +27,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define AUTO_MOUSE_DEFAULT_LAYER 1
 #define COMBO_HOLD_TERM  120
 #define TAPPING_TERM 120        // デフォルト200 → 120msなど短く
+#define COMBO_TERM  30          // デフォルト50ms
 #define IGNORE_MOD_TAP_INTERRUPT
