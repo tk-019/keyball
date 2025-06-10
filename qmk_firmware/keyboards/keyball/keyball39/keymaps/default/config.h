82f202c547e57bebaccf0b048ecccd769c4533e2
gunshop-eikoh <toshiki-komatsu@eikoh-gun.com>
2025-06-11 00:14:02 +0900

    keymap: revert TAPPING_TERM to 110ms and add PERMISSIVE_HOLD definition

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/default/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/default/config.h
@@ -26,5 +26,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 1
 #define COMBO_HOLD_TERM  120
-// ホールド判定を 120 ms に短縮
-#define TAPPING_TERM 120
+#define TAPPING_TERM 110        // デフォルト200 → 120msなど短く
+#define PERMISSIVE_HOLD         // 他キーを押した瞬間ホールド確定
