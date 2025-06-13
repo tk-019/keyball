85eb5d5048b642313f20cacaf25d7ba83dcaded3
gunshop-eikoh <toshiki-komatsu@eikoh-gun.com>
2025-06-14 03:18:23 +0900

    keymap: update key mappings and adjust layer configurations for improved functionality

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/default/keymap.c
@@ -27,33 +27,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
     LCTL_T(KC_A)     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , KC_ENT  ,
     LSFT_T(KC_Z)  , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , LT(3,KC_M)     , KC_COMM  , KC_DOT   , RSFT_T(LSFT(KC_SLSH))  ,
-    KC_ESC  ,  KC_LGUI  ,KC_LALT
+    KC_LSFT  ,  KC_LGUI  ,KC_LALT
     // 左親指
     ,LT(1,KC_LNG2),KC_LCTL,LT(2,KC_SPC)
     // 右親指
     ,LT(1,KC_SPC),LT(2,KC_LNG1)
     // 右下端
-    , LT(3,LSFT(KC_INT1))
+    , KC_ESC
+    //, LT(3,LSFT(KC_INT1))
   ),
   // 左移動とファンクション
   [1] = LAYOUT(
     KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5  ,                            KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   ,
     KC_LCTL    , LCTL(LGUI(KC_LEFT))  , LGUI(KC_TAB)  ,LCTL(LGUI(KC_RGHT)), LSFT(KC_8)  ,                           LSFT(KC_INT1), KC_BTN1  , KC_PGUP  , KC_BTN2  , KC_ENT  ,
     KC_LSFT, LALT(KC_LEFT),LALT(KC_TAB)   , LALT(KC_RGHT)  ,LSFT(KC_RBRC),                            KC_LBRC  , KC_DLR   , KC_PGDN  , KC_BTN3  , RSFT_T(KC_F11)   ,
-    KC_ESC  ,  KC_LGUI  ,KC_LALT  ,  _______    ,KC_LCTL, _______  ,      TO(2)    , TO(0) ,                                              KC_F12
+    KC_LSFT  ,  KC_LGUI  ,KC_LALT  ,  _______    ,KC_LCTL, _______  ,      TO(2)    , TO(0) ,                                              KC_F12
   ),
   // 右移動＋マウスと左数字
   [2] = LAYOUT(
     KC_NUM_LOCK   , KC_KP_7     , KC_KP_8     , KC_KP_9     , KC_MINS  ,                            KC_PGUP  , KC_HOME  , KC_UP  , KC_END  , KC_PGDN  ,
     KC_LCTL, KC_KP_4     , KC_KP_5     , KC_KP_6     ,KC_COMM,                            LSFT(KC_9)  , KC_LEFT  , KC_DOWN    , KC_RGHT  , KC_ENT  ,
     KC_LSFT , KC_KP_1     , KC_KP_2     , KC_KP_3     ,KC_DOT,                           KC_BTN3, KC_BTN1  , KC_BTN2  ,  _______ , KC_RSFT  ,
-    KC_ESC   , KC_LGUI     , KC_LALT   ,  KC_KP_0   ,KC_LCTL, KC_KP_0     ,      _______  , _______,                                   _______
+    KC_LSFT   , KC_LGUI     , KC_LALT   ,  KC_KP_0   ,KC_LCTL, KC_KP_0     ,      _______  , _______,                                   _______
   ),
   [3] = LAYOUT(
     RGB_TOG  , AML_TO   , AML_I50  , AML_D50  , _______  ,                            _______  , _______  , SSNP_HOR , SSNP_VRT , SSNP_FRE ,
-    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , SCRL_DVI ,                            _______  , _______  , _______  , _______  , _______  ,
-    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , SCRL_DVD ,                            CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , KBC_SAVE ,
-    QK_BOOT  , KBC_RST  , _______  , _______  , _______  , _______  ,      _______  , _______                                   , QK_BOOT
+    KC_LCTL  , RGB_HUI  , RGB_SAI  , RGB_VAI  , SCRL_DVI ,                            _______  , _______  , _______  , _______  , _______  ,
+    KC_LSFT , RGB_HUD  , RGB_SAD  , RGB_VAD  , SCRL_DVD ,                            CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , KBC_SAVE ,
+    KC_LSFT  , KC_LGUI  , KC_LALT  , _______  , _______  , _______  ,      _______  , _______                                   , QK_BOOT
   ),
 };
 // clang-format on
@@ -77,6 +78,7 @@ void oledkit_render_info_user(void) {
 
 
 #ifdef COMBO_ENABLE
+const uint16_t PROGMEM combo_commDot[] = {KC_COMM, KC_DOT, COMBO_END};      // _
 const uint16_t PROGMEM combo_io[] = {KC_I, KC_O, COMBO_END};      // bs
 const uint16_t PROGMEM combo_ui[] = {KC_U, KC_I, COMBO_END};      // del
 const uint16_t PROGMEM combo_we[] = {KC_W, KC_E, COMBO_END};      // tab
@@ -111,6 +113,7 @@ const uint16_t PROGMEM combo_dotF[] = {KC_DOT, KC_F, COMBO_END};    // ]
 const uint16_t PROGMEM combo_f_[] = {KC_F, LT(3,LSFT(KC_INT1)), COMBO_END};    // ¥
 
 combo_t key_combos[] = {
+    COMBO(combo_commDot, LSFT(KC_INT1)), // _
     COMBO(combo_io, KC_BSPC), // bs
     COMBO(combo_ui, KC_DELETE), // del
     COMBO(combo_we, KC_TAB), // tab
