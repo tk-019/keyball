3b559904780baed02f38f03d5f6b96871b271109
gunshop-eikoh <toshiki-komatsu@eikoh-gun.com>
2025-06-16 02:07:42 +0900

    keymap: adjust layer 4 layout for improved key accessibility

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/default/keymap.c
@@ -41,9 +41,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC  , KC_LGUI  , KC_LALT  , _______  , _______  , _______  ,      _______  , _______                                   , QK_BOOT
   ),
   [4] = LAYOUT(
-    KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                            KC_7     , KC_8     , KC_9     , _______  , KC_BSPC  ,
-    KC_LCTL  , KC_F6    , KC_F7    , KC_F8    , KC_F9    ,                            KC_4     , KC_5     , KC_6     , _______  , KC_ENT   ,
-    KC_LSFT  , KC_F10   , KC_F11   , KC_F12   , _______  ,                            KC_1     , KC_2     , KC_3     , KC_0     , KC_RSFT  ,
+    KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                            _______  , KC_7     , KC_8     , KC_9     , KC_BSPC  ,
+    KC_LCTL  , KC_F6    , KC_F7    , KC_F8    , KC_F9    ,                            KC_0     , KC_4     , KC_5     , KC_6     , KC_ENT   ,
+    KC_LSFT  , KC_F10   , KC_F11   , KC_F12   , _______  ,                            KC_0     , KC_1     , KC_2     , KC_3     , KC_RSFT  ,
     KC_ESC   , KC_LGUI  , KC_LALT  , _______  , _______  , _______  ,      _______     , _______                                    , _______
   ),
 };
